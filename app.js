ad03c87acba32e219f3b865b321158856c7b96b7
antoineterny <63339704+antoineterny@users.noreply.github.com>
2022-02-11 17:47:22 +0100

    Tout est fonctionnel

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
 var metronome = new Metronome()
-var tempo = document.getElementById("tempo")
-tempo.textContent = metronome.tempo
+var tempoInput = document.getElementById("tempo-input")
+tempoInput.value = metronome.displayedTempo
 
-var playPauseIcon = document.getElementById("play-pause-icon")
+const maelzelDiv = document.querySelector("#maelzel")
+const uniteDiv = document.querySelector("#unite")
 
-var playButton = document.getElementById("play-button")
-playButton.addEventListener("click", function () {
+const selectorCheckbox = document.querySelector("#selector-checkbox")
+selectorCheckbox.onclick = () => {
+  maelzelDiv.classList.toggle("active")
+  uniteDiv.classList.toggle("active")
+}
+maelzelDiv.onclick = () => {
+  maelzelDiv.classList.add("active")
+  uniteDiv.classList.remove("active")
+  selectorCheckbox.checked = false
+}
+uniteDiv.onclick = () => {
+  uniteDiv.classList.add("active")
+  maelzelDiv.classList.remove("active")
+  selectorCheckbox.checked = true
+}
+
+const startStopMetronome = function () {
   metronome.startStop()
-
-  if (metronome.isRunning) {
-    playPauseIcon.className = "pause"
-  } else {
-    playPauseIcon.className = "play"
-  }
+  document.querySelector("#play").classList.toggle("invisible")
+  document.querySelector("#stop").classList.toggle("invisible")
+}
+document.querySelectorAll(".play-stop-buttons button").forEach(btn => {
+  btn.addEventListener("click", startStopMetronome)
 })
+document.addEventListener("keypress", e => {
+  if (e.code === "Enter" || e.code === "Space" || e.code === "NumpadEnter") startStopMetronome()
+})
+
+tempoInput.onchange = () => {
+  if (tempoInput.value > 250) tempoInput.value = 250
+  metronome.displayedTempo = tempoInput.value
+}
 
 const maelzel = [
   40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 63, 66, 69, 72, 76, 80, 84, 88, 92, 96, 100, 104, 108,
@@ -34,22 +57,41 @@ const prevMaelzel = currentTempo => {
   return 40
 }
 
-var tempoChangeButtons = document.getElementsByClassName("tempo-change")
-for (var i = 0; i < tempoChangeButtons.length; i++) {
-  tempoChangeButtons[i].addEventListener("click", function () {
-    metronome.tempo += parseInt(this.dataset.change)
-    tempo.textContent = metronome.tempo
+const plus = document.querySelector("#plus")
+plus.onclick = () => {
+  metronome.displayedTempo = selectorCheckbox.checked
+    ? metronome.displayedTempo + 1
+    : nextMaelzel(metronome.displayedTempo)
+  tempoInput.value = metronome.displayedTempo
+}
+
+const moins = document.querySelector("#moins")
+moins.onclick = () => {
+  metronome.displayedTempo = selectorCheckbox.checked
+    ? metronome.displayedTempo - 1
+    : prevMaelzel(metronome.displayedTempo)
+  tempoInput.value = metronome.displayedTempo
+}
+
+const selectSubdiv = int => {
+  metronome.subdivisions = int
+  const subdivBtns = document.querySelectorAll(".subdiv")
+  subdivBtns.forEach((sub, i) => {
+    if (i < int) sub.classList.add("active")
+    else sub.classList.remove("active")
   })
 }
 
-const nextMaelzelBtn = document.querySelector('#next-maelzel')
-nextMaelzelBtn.onclick = () => {
-  metronome.tempo = nextMaelzel(metronome.tempo)
-  tempo.textContent = metronome.tempo
-}
-
-const prevMaelzelBtn = document.querySelector('#prev-maelzel')
-prevMaelzelBtn.onclick = () => {
-  metronome.tempo = prevMaelzel(metronome.tempo)
-  tempo.textContent = metronome.tempo
+const taps = [Date.now()]
+function tapTempo() {
+  taps.push(Date.now())
+  console.log('taps', taps)
+  let diff =
+    ((taps[taps.length - 1] - taps[taps.length - 2]) +
+      (taps[taps.length - 2] - taps[taps.length - 3])) /
+    2
+  let bpm = Math.round(60000 / diff)
+  if (bpm > 30) {
+    metronome.displayedTempo = tempoInput.value = bpm
+  } 
 }
--- metronome.js
+++ metronome.js
@@ -2,30 +2,33 @@ class Metronome {
   constructor(tempo = 88) {
     this.audioContext = null
     this.notesInQueue = [] // notes that have been put into the web audio and may or may not have been played yet {note, time}
-    this.currentQuarterNote = 0
-    this.tempo = tempo
+    this.displayedTempo = tempo
+    this.subdivisions = 1
+    this.currentSubdiv = 0
     this.lookahead = 25 // How frequently to call scheduling function (in milliseconds)
     this.scheduleAheadTime = 0.1 // How far ahead to schedule audio (sec)
     this.nextNoteTime = 0.0 // when the next note is due
     this.isRunning = false
     this.intervalID = null
     this.buffers = {}
+    this.soundName = "clave"
   }
 
   nextNote() {
-    // Advance current note and time by a quarter note (crotchet if you're posh)
-    var secondsPerBeat = 60.0 / this.tempo // Notice this picks up the CURRENT tempo value to calculate beat length.
+    this.actualTempo = this.displayedTempo * this.subdivisions
+    // Advance current note and time by a subdivision
+    const secondsPerBeat = 60.0 / this.actualTempo // Notice this picks up the CURRENT tempo value to calculate beat length.
     this.nextNoteTime += secondsPerBeat // Add beat length to last beat time
 
-    this.currentQuarterNote++ // Advance the beat number, wrap to zero
-    if (this.currentQuarterNote == 4) {
-      this.currentQuarterNote = 0
+    this.currentSubdiv++ // Advance the subdiv number, wrap to zero
+    if (this.currentSubdiv == this.subdivisions) {
+      this.currentSubdiv = 0
     }
   }
 
-  scheduleNote(beatNumber, time) {
+  scheduleNote(subdivNumber, time) {
     // push the note on the queue, even if we're not playing.
-    this.notesInQueue.push({ note: beatNumber, time: time })
+    this.notesInQueue.push({ note: subdivNumber, time: time })
 
     // create an oscillator
     // const osc = this.audioContext.createOscillator()
@@ -34,22 +37,10 @@ class Metronome {
     const gainNode = this.audioContext.createGain()
     source.buffer = this.buffers.clave
 
-    // // osc.frequency.value = (beatNumber % 4 == 0) ? 1000 : 800;
-    // osc.frequency.value = 800
-    // envelope.gain.value = 1
-    // envelope.gain.exponentialRampToValueAtTime(1, time + 0.001)
-    // envelope.gain.exponentialRampToValueAtTime(0.001, time + 0.02)
-
-    // osc.connect(envelope)
-    // envelope.connect(this.audioContext.destination)
-
-    const vol = beatNumber % 4 == 0 ? 1 : .25
+    const vol = subdivNumber % this.subdivisions == 0 ? 1 : 0.1
     source.connect(gainNode)
     gainNode.gain.value = vol
     gainNode.connect(this.audioContext.destination)
-
-    // osc.start(time)
-    // osc.stop(time + 0.03)
     
     source.start(time)
     source.stop(time + 0.3)
@@ -58,7 +49,7 @@ class Metronome {
   scheduler() {
     // while there are notes that will need to play before the next interval, schedule them and advance the pointer.
     while (this.nextNoteTime < this.audioContext.currentTime + this.scheduleAheadTime) {
-      this.scheduleNote(this.currentQuarterNote, this.nextNoteTime)
+      this.scheduleNote(this.currentSubdiv, this.nextNoteTime)
       this.nextNote()
     }
   }
@@ -68,12 +59,12 @@ class Metronome {
 
     if (this.audioContext == null) {
       this.audioContext = new (window.AudioContext || window.webkitAudioContext)()
-      this.loadSound("clave", this.audioContext, this.buffers)
+      this.loadSound(this.soundName, this.audioContext, this.buffers)
     }
 
     this.isRunning = true
 
-    this.currentQuarterNote = 0
+    this.currentSubdiv = 0
     this.nextNoteTime = this.audioContext.currentTime + 0.05
 
     this.intervalID = setInterval(() => this.scheduler(), this.lookahead)
@@ -94,7 +85,7 @@ class Metronome {
   }
 
   loadSound(fileName, context, buffers) {
-    fetch(`./${fileName}.mp3`)
+    fetch(`./snd/${fileName}.mp3`)
       .then(data => data.arrayBuffer())
       .then(arrayBuffer => context.decodeAudioData(arrayBuffer))
       .then(decodedAudio => {
